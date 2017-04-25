61f8704258321968fab653939e1ea2129d7ad149
wangnz <5323700@qq.com>
2017-04-25 17:34:34 +0800

    update

--- src/main/java/com/wangnz/myem/test/controller/TestController.java
+++ src/main/java/com/wangnz/myem/test/controller/TestController.java
@@ -2,9 +2,17 @@ package com.wangnz.myem.test.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Administrator on 2017/4/21.
@@ -13,10 +21,41 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class TestController {
     private Logger log = LoggerFactory.getLogger(this.getClass());
 
+    @Resource(name = "stringRedisTemplate")
+    private StringRedisTemplate stringTemplate;
+
     @RequestMapping(value = "/test1")
     @ResponseBody
     public String test1() {
-        log.info("oks");
+        log.info("info");
+        log.debug("debug");
+
+//        template.execute(new RedisCallback<Boolean>() {
+//            @Override
+//            public Boolean doInRedis(RedisConnection connection) throws DataAccessException {
+//                byte [] key = "tempkey".getBytes();
+//                byte[] value = "tempvalue".getBytes();
+//                connection.set(key, value);
+//                return true;
+//            }
+//        });
+        stringTemplate.opsForValue().set("city", "上海");
+        log.info(stringTemplate.opsForValue().get("city"));
+        return "ok";
+    }
+
+    @RequestMapping(value = "/test2")
+    @ResponseBody
+    public String test2() {
+        log.info("info");
+        JedisPoolConfig config = new JedisPoolConfig();
+        config.setMaxTotal(100);
+        config.setMaxIdle(10);
+        config.setMaxWaitMillis(3000);
+        config.setTestOnBorrow(true);
+        JedisPool jedisPool = new JedisPool(config, "192.168.182.129", 6379, 3000);
+        Jedis jedis = jedisPool.getResource();
+        jedis.set("tempkey", "tempvalue");
         return "ok";
     }
 }
